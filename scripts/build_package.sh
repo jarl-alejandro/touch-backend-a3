e86d046bc8be297d779df851f1c8fc48d2a5bece
Alejandro Rivas <jarlalejor@gmail.com>
2020-02-17 12:03:39 -0500

    compile

--- scripts/build_package.sh
+++ scripts/build_package.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -e
 npx tsc
-npx babel --config-file=../../../babel.config.js lib --ignore="src/**/__tests__/**" --out-dir dist/esm &
-npx babel --config-file=../../../babel.config.cjs.js lib --ignore="src/**/__tests__/**" --out-dir dist/cjs &
+
+npx babel --config-file=babel.config.js lib --ignore="src/**/__tests__/**" --out-dir dist/esm &
+npx babel --config-file=babel.config.cjs.js lib --ignore="src/**/__tests__/**" --out-dir dist/cjs &
 wait
 
 if [ -f ./rollup.config.js ]
